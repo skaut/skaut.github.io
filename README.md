731d4458e429750b6de133caf094a025dbe4266a
Jindrich Pilar <pilar.jindrich@gmail.com>
2014-11-30 13:08:34 -0500

    Generovani dokumentace

--- README.md
+++ README.md
@@ -2,8 +2,8 @@
 
 Web pro GitHub [organizaci](https://github.com/skaut)
 
-##Generovani API
-Misto nastavovani generatoru pro kazdy repozitar skript (generateApiDoc.sh)[./generateApiDoc.sh] stahne vsechny repozitare organizace githu.com/skaut a vygeneruje jeich API
+##Generovani
+Misto nastavovani generatoru pro kazdy repozitar skript [generateApiDoc.sh](./generateApiDoc.sh) stahne vsechny repozitare organizace githu.com/skaut a vygeneruje jeich API
 
 ###Vyhody
 Nikdo nemusi nic konfigurovat, pridavat, vse resi tento projekt
@@ -11,3 +11,14 @@ Nikdo nemusi nic konfigurovat, pridavat, vse resi tento projekt
 ###Nevyhody
 * Rucni spusteni (urcite pujde zautomatizovat)
 * Nepodporuje verzovani
+* Nepodporuje vice BRANCHu
+
+##Generovani dokumentace (ukazky, navody)
+Pro generovani dokumentace se pouziva [mkdocs](http://www.mkdocs.org/).
+Je to nastroj ktery parsuje Markdown soubory ve slozce docs nachazejici se v root adresaro repozitare.
+
+##Generovani API
+Pro generovani API se pouziva [apigen](http://apigen.org/)
+Je to nastroj ktery parsuje ``@phpdocs``
+
+
--- generateApiDoc.sh
+++ generateApiDoc.sh
@@ -7,15 +7,40 @@ urls=$(curl -s https://api.github.com/users/skaut/repos | grep git_url | cut -d 
 rm -rf api;
 mkdir api;
 
-for url in $urls; do
+rm -rf docs;
+mkdir docs;
 
-	repName=$(echo "$url" | rev | cut -d/ -f1 | cut -d. -f2- | rev)
-	git clone "$url" tmp;
+for repoUrl in $urls; do
+
+	repName=$(echo "$repoUrl" | rev | cut -d/ -f1 | cut -d. -f2- | rev)
+
+	if [ "$repName" == "skaut.github.io" ]; then
+		continue;
+	fi
+
+	rm -rf tmp;
+	git clone "$repoUrl" tmp;
 
 	#Upravi a nakopiruje konfigurak
 	cat config.neon | sed "s/title: .*/title: $repName/" > tmp/config.neon
 
+	#Generate API reference
 	apigen --source tmp --destination "api/$repName"
+
+	#Generate docs
+	docUrl="http:\/\/skaut.github.io\/docs\/$repName"
+	mkdir -p "docs/$repName"
+	repoUrl="" #TODO escpe / for sed
+	cat mkdocs.yml | sed "s/site_name:.*/site_name: $repName/" | sed "s/site_url:.*/site_url: $docUrl/" | sed "s/repo_url:.*/repo_url: $repoUrl/" | sed "s/site_dir:.*/site_dir: ..\/docs\/$repName/" > tmp/mkdocs.yml
+
+
+	docsNotExist=$(ls -l tmp/docs | grep ".md" | wc -l)
+	if [ $docsNotExist -eq 0 ]; then
+		mkdir -p tmp/docs;
+		echo "#Skript generujici dokumentaci nenasel soubory. Ujistete se ze maji .md priponu a jsou v adresi docs ktery je v rootu git repositare"  > tmp/docs/index.md
+	fi
+
+	cd tmp && mkdocs build && cd -;
 
 	rm -rf tmp;
 done;
